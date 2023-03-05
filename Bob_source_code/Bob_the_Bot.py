809d9342864275687328046de9943ce4628e3c06
victor <vdcommits@outlook.com>
2023-03-05 22:20:42 +0100

    Auto reformat + add missing command handling to reduce errors in log

--- Bob_source_code/Bob_the_Bot.py
+++ Bob_source_code/Bob_the_Bot.py
@@ -12,6 +12,7 @@ import discord
 import datetime
 import calendar
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -22,23 +23,25 @@ intents.typing = False
 intents.presences = False
 intents.message_content = True
 
-from discord.ext import commands
+bot = commands.Bot(command_prefix='?', intents=intents)
 
-bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_connect():
     print("Bot connected")
 
+
 @bot.event
 async def on_ready():
     print("Bob is ready!")
     await bot.get_channel(DISCORD_SERVER_CHANNEL).send("Bob is ready!")
-    
+
+
 @bot.command(name='greet')
 async def greet(ctx):
     print(f"<greet> command called by {ctx.author.name} in {ctx.channel.name} at {datetime.datetime.utcnow()} UTC")
     await ctx.send(f"Hi there {ctx.author.mention}!")
+
 
 @bot.command(name='creator')
 async def creator(ctx):
@@ -49,24 +52,33 @@ async def creator(ctx):
     # creapy01= discord.User(id='<@688448403939655680>')
     await ctx.send("<@688448403939655680> is my creator!")
 
+
 def short2longDAY(day):
     """Turns a day from short name (Mon) into long name (Monday)."""
-    if day=='Mon': return 'Monday'
-    elif day=='Tue': return 'Tuesday'
-    elif day=='Wed': return 'Wednesday'
-    elif day=='Thu': return 'Thursday'
-    elif day=='Fri': return 'Friday'
-    elif day=='Sat': return 'Saturday'
-    elif day=='Sun': return 'Sunday'
+    if day == 'Mon':
+        return 'Monday'
+    elif day == 'Tue':
+        return 'Tuesday'
+    elif day == 'Wed':
+        return 'Wednesday'
+    elif day == 'Thu':
+        return 'Thursday'
+    elif day == 'Fri':
+        return 'Friday'
+    elif day == 'Sat':
+        return 'Saturday'
+    elif day == 'Sun':
+        return 'Sunday'
     else:
         return None
 
+
 @bot.command(name='roll_call')
 async def roll_call(ctx, start_time_utc=18, slot1react=':green_apple:',
-                                        slot2react=':apple:',
-                                        slot3react=':pear:',
-                                        slot4react=':tangerine:',
-                                        absentreact=':cucumber:'):
+                    slot2react=':apple:',
+                    slot3react=':pear:',
+                    slot4react=':tangerine:',
+                    absentreact=':cucumber:'):
     """
     Generates the sequence of messages for the roll call. Automatically adds reactions to them.
     :param ctx:
@@ -81,31 +93,39 @@ async def roll_call(ctx, start_time_utc=18, slot1react=':green_apple:',
     print(f"<roll_call> command called by {ctx.author.name} in {ctx.channel.name} at {datetime.datetime.utcnow()} UTC")
     await ctx.message.delete()
     reacts = [slot1react, slot2react, slot3react, slot4react, absentreact]
-    days_list = ['Wed', 'Thu', 'Sat', 'Sun']   # list of CB days - from 'Mon Tue Wed Thu Fri Sat Sun'
+    days_list = ['Wed', 'Thu', 'Sat', 'Sun']  # list of CB days - from 'Mon Tue Wed Thu Fri Sat Sun'
     today = datetime.datetime.utcnow()
     first_msg = True
     first_day_week = False
     for i in range(12):
         if days_list == []: break
         print(f"scanning {today.strftime('%a %w %b %Y')} with first CB day = {days_list[0]}")
-        if today.strftime("%a") == days_list[0]:    # check that the first CB day of the week has been found
+        if today.strftime("%a") == days_list[0]:  # check that the first CB day of the week has been found
             first_day_week = True
-        if ((today.strftime("%a") in days_list) and first_day_week):
+        if (today.strftime("%a") in days_list) and first_day_week:
             days_list = days_list[1:]
             day_title = today.strftime("%A, %d %B %Y")
-            timemarker1 = calendar.timegm(today.replace(hour=start_time_utc, minute=0, second=0, microsecond=0).timetuple())
-            timemarker2 = calendar.timegm(today.replace(hour=start_time_utc+1, minute=0, second=0, microsecond=0).timetuple())
-            timemarker3 = calendar.timegm(today.replace(hour=start_time_utc+2, minute=0, second=0, microsecond=0).timetuple())
-            timemarker4 = calendar.timegm(today.replace(hour=start_time_utc+3, minute=0, second=0, microsecond=0).timetuple())
-            timemarker5 = calendar.timegm(today.replace(hour=start_time_utc+4, minute=0, second=0, microsecond=0).timetuple())
+            timemarker1 = calendar.timegm(
+                today.replace(hour=start_time_utc, minute=0, second=0, microsecond=0).timetuple())
+            timemarker2 = calendar.timegm(
+                today.replace(hour=start_time_utc + 1, minute=0, second=0, microsecond=0).timetuple())
+            timemarker3 = calendar.timegm(
+                today.replace(hour=start_time_utc + 2, minute=0, second=0, microsecond=0).timetuple())
+            timemarker4 = calendar.timegm(
+                today.replace(hour=start_time_utc + 3, minute=0, second=0, microsecond=0).timetuple())
+            timemarker5 = calendar.timegm(
+                today.replace(hour=start_time_utc + 4, minute=0, second=0, microsecond=0).timetuple())
             if first_msg:
-                message = await ctx.send(f"This is the roll-call for **{day_title}**. Please react accordingly if you want to play in the respective time slots (times will be displayed as your local time if your device is set to the correct time zone):\n\n<t:{timemarker1}> to <t:{timemarker2}>: {slot1react}\n<t:{timemarker2}> to <t:{timemarker3}>: {slot2react}\n<t:{timemarker3}> to <t:{timemarker4}>: {slot3react}\n<t:{timemarker4}> to <t:{timemarker5}>: {slot4react}\nCannot make it: {absentreact}")
+                message = await ctx.send(
+                    f"This is the roll-call for **{day_title}**. Please react accordingly if you want to play in the respective time slots (times will be displayed as your local time if your device is set to the correct time zone):\n\n<t:{timemarker1}> to <t:{timemarker2}>: {slot1react}\n<t:{timemarker2}> to <t:{timemarker3}>: {slot2react}\n<t:{timemarker3}> to <t:{timemarker4}>: {slot3react}\n<t:{timemarker4}> to <t:{timemarker5}>: {slot4react}\nCannot make it: {absentreact}")
                 first_msg = False
             else:
-                message = await ctx.send(f"#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#\nThis is the roll-call for **{day_title}**. Please react accordingly if you want to play in the respective time slots (times will be displayed as your local time if your device is set to the correct time zone):\n\n<t:{timemarker1}> to <t:{timemarker2}>: {slot1react}\n<t:{timemarker2}> to <t:{timemarker3}>: {slot2react}\n<t:{timemarker3}> to <t:{timemarker4}>: {slot3react}\n<t:{timemarker4}> to <t:{timemarker5}>: {slot4react}\nCannot make it: {absentreact}")
+                message = await ctx.send(
+                    f"#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#\nThis is the roll-call for **{day_title}**. Please react accordingly if you want to play in the respective time slots (times will be displayed as your local time if your device is set to the correct time zone):\n\n<t:{timemarker1}> to <t:{timemarker2}>: {slot1react}\n<t:{timemarker2}> to <t:{timemarker3}>: {slot2react}\n<t:{timemarker3}> to <t:{timemarker4}>: {slot3react}\n<t:{timemarker4}> to <t:{timemarker5}>: {slot4react}\nCannot make it: {absentreact}")
             for react in reacts:
                 await message.add_reaction(react)
         today += datetime.timedelta(days=1)
+
 
 @bot.command(name='helpmebob')
 async def helpmebob(ctx):
@@ -115,7 +135,20 @@ async def helpmebob(ctx):
     :return: Void
     """
     print(f"<helpmebob> command called by {ctx.author.name} in {ctx.channel.name} at {datetime.datetime.utcnow()} UTC")
-    await ctx.send("Bob is a bot. And does Bob the Bot do?\nBob the Bot does your beloved CB roll call.\nBut how can I get Bob the Bot to do what Bob the Bot does?\nAt the beginning of the CB week, type `/roll_call` followed by the UTC starting time of the CB session, and 5 emotes for people to react to.\nAnd then, sit and watch :wink:")
+    await ctx.send(
+        "Bob is a bot. And does Bob the Bot do?\n"
+        "Bob the Bot does your beloved CB roll call.\n"
+        "But how can I get Bob the Bot to do what Bob the Bot does?\n"
+        "At the beginning of the CB week, type `/roll_call` followed by the UTC "
+        "starting time of the CB session, and 5 emotes for people to react to.\n"
+        "And then, sit and watch :wink:")
 
-#%% Run Bob_the_Bot
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.CommandNotFound):
+        await ctx.send("That command wasn't found! Sorry :(")
+
+
+# %% Run Bob_the_Bot
 bot.run(TOKEN)
