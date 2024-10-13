23643a0fcc8c94db78f398adfc6eee186ae8aaa6
LoJe-24-8 <loisjean2001@gmail.com>
2024-10-14 01:22:45 +0200

    Added parameters and simplified main source for readability

--- Bob_source_code/Bob_the_Bot.py
+++ Bob_source_code/Bob_the_Bot.py
@@ -11,6 +11,7 @@ import os
 import discord
 import datetime
 import calendar
+from parametersLoader import Parameters
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -24,6 +25,10 @@ intents.presences = False
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='?', intents=intents)
+
+parameters = Parameters()
+dv = parameters.getDefaultValues()
+settings = parameters.getSettings()
 
 
 @bot.event
@@ -74,7 +79,8 @@ def short2longDAY(day):
 
 
 @bot.command(name='roll_call')
-async def roll_call(ctx, start_time_utc=18, slot1react=':green_apple:',
+async def roll_call(ctx, start_time_utc=dv.get('default_start_time_utc'),
+                    slot1react=':green_apple:',
                     slot2react=':apple:',
                     slot3react=':pear:',
                     slot4react=':tangerine:',
@@ -93,7 +99,7 @@ async def roll_call(ctx, start_time_utc=18, slot1react=':green_apple:',
     print(f"<roll_call> command called by {ctx.author.name} in {ctx.channel.name} at {datetime.datetime.utcnow()} UTC")
     await ctx.message.delete()
     reacts = [slot1react, slot2react, slot3react, slot4react, absentreact]
-    days_list = ['Wed', 'Thu', 'Sat', 'Sun']  # list of CB days - from 'Mon Tue Wed Thu Fri Sat Sun'
+    days_list = dv.get('days_list')
     today = datetime.datetime.utcnow()
     first_msg = True
     first_day_week = False
@@ -103,36 +109,34 @@ async def roll_call(ctx, start_time_utc=18, slot1react=':green_apple:',
         print(f"scanning {today.strftime('%a %w %b %Y')} with first CB day = {days_list[0]}")
         if today.strftime("%a") == days_list[0]:  # check that the first CB day of the week has been found
             first_day_week = True
-        if (today.strftime("%a") in days_list) and first_day_week:
-            days_list = days_list[1:]
+        if (today.strftime("%a") in days_list) and first_day_week:  # TODO: this line and the previous one are somewhat useless - if replace with a break instruction at the end of this if block
+            days_list = days_list[1:]  # useless
             day_title = today.strftime("%A, %d %B %Y")
-            timemarker1 = calendar.timegm(
-                today.replace(hour=start_time_utc, minute=0, second=0, microsecond=0).timetuple())
-            timemarker2 = calendar.timegm(
-                today.replace(hour=start_time_utc + 1, minute=0, second=0, microsecond=0).timetuple())
-            timemarker3 = calendar.timegm(
-                today.replace(hour=start_time_utc + 2, minute=0, second=0, microsecond=0).timetuple())
-            timemarker4 = calendar.timegm(
-                today.replace(hour=start_time_utc + 3, minute=0, second=0, microsecond=0).timetuple())
-            timemarker5 = calendar.timegm(
-                today.replace(hour=start_time_utc + 4, minute=0, second=0, microsecond=0).timetuple())
+            generate_timemarker = lambda h: calendar.timegm(today.replace(hour=start_time_utc + h,
+                                                                          minute=dv.get('minute_offset'),
+                                                                          second=dv.get('second_offset'),
+                                                                          microsecond=dv.get('microsecond_offset'))
+                                                            .timetuple())
+            timemarker1 = generate_timemarker(0)
+            timemarker2 = generate_timemarker(1)
+            timemarker3 = generate_timemarker(2)
+            timemarker4 = generate_timemarker(3)
+            timemarker5 = generate_timemarker(4)
+
+            message_body = (f"This is the roll-call for **{day_title}**. Please react accordingly if you want to play "
+                            f"in the respective time slots (times will be displayed as your local time if your device "
+                            f"is set to the correct time zone):\n\n"
+                            f"<t:{timemarker1}> to <t:{timemarker2}>: {slot1react}\n"
+                            f"<t:{timemarker2}> to <t:{timemarker3}>: {slot2react}\n"
+                            f"<t:{timemarker3}> to <t:{timemarker4}>: {slot3react}\n"
+                            f"<t:{timemarker4}> to <t:{timemarker5}>: {slot4react}\n"
+                            f"Cannot make it: {absentreact}")
+            message_separator = "#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#\n"
             if first_msg:
-                message = await ctx.send(
-                    f"This is the roll-call for **{day_title}**. Please react accordingly if you want to play in the "
-                    f"respective time slots (times will be displayed as your local time if your device is set to the "
-                    f"correct time zone):\n\n<t:{timemarker1}> to <t:{timemarker2}>: {slot1react}\n<t:{timemarker2}> "
-                    f"to <t:{timemarker3}>: {slot2react}\n<t:{timemarker3}> to <t:{timemarker4}>: {slot3react}\n<t:"
-                    f"{timemarker4}> to <t:{timemarker5}>: {slot4react}\nCannot make it: {absentreact}")
+                message = await ctx.send(message_body)
                 first_msg = False
             else:
-                message = await ctx.send(
-                    f"#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#\n"
-                    f"This is the roll-call for **{day_title}**. Please react "
-                    f"accordingly if you want to play in the respective time slots (times will be displayed as your "
-                    f"local time if your device is set to the correct time zone):\n\n<t:{timemarker1}> to <t:"
-                    f"{timemarker2}>: {slot1react}\n<t:{timemarker2}> to <t:{timemarker3}>: {slot2react}\n<t:"
-                    f"{timemarker3}> to <t:{timemarker4}>: {slot3react}\n<t:{timemarker4}> to <t:{timemarker5}>: "
-                    f"{slot4react}\nCannot make it: {absentreact}")
+                message = await ctx.send(message_separator + message_body)
             for react in reacts:
                 await message.add_reaction(react)
         today += datetime.timedelta(days=1)
@@ -146,13 +150,7 @@ async def helpmebob(ctx):
     :return: Void
     """
     print(f"<helpmebob> command called by {ctx.author.name} in {ctx.channel.name} at {datetime.datetime.utcnow()} UTC")
-    await ctx.send(
-        "Bob is a bot. And does Bob the Bot do?\n"
-        "Bob the Bot does your beloved CB roll call.\n"
-        "But how can I get Bob the Bot to do what Bob the Bot does?\n"
-        "At the beginning of the CB week, type `/roll_call` followed by the UTC "
-        "starting time of the CB session, and 5 emotes for people to react to.\n"
-        "And then, sit and watch :wink:")
+    await ctx.send(dv.get('help_message'))
 
 
 @bot.event
---
+++ Bob_source_code/parametersLoader.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+This file imports the parameters values defined in the config/parameters.json file
+"""
+import json
+
+class Parameters():
+    def __init__(self):
+        self.paramFilePath = "config/"
+        self.paramFileName = "parameters.json"
+        self.settings = {}
+        self.defaultValues = {}
+        self.ReadParameters()
+
+    def __str__(self):
+        return str(f'Settings : {self.settings}\nDefault values: {self.defaultValues}')
+
+    def ReadParameters(self):
+        print(f'Reading parameters from config file {self.paramFilePath + self.paramFileName} :')
+        try:
+            with open(self.paramFilePath + self.paramFileName, 'r') as file:
+                data = json.load(file)
+                self.settings = {}
+                self.defaultValues = {}
+
+                for setting in data['settings']:
+                    if setting['name'] is not None:
+                        if setting['name'] in self.settings.keys():
+                            print(f'Setting {setting['name']} is duplicated in '
+                                  f'{self.paramFilePath}{self.paramFileName}, value will be overwritten.')
+                        else:
+                            self.settings[setting['name']] = setting['value']
+
+                for defaultValue in data['default_values']:
+                    if defaultValue['name'] is not None:
+                        if defaultValue['name'] in self.defaultValues.keys():
+                            print(f'Default value {defaultValue['name']} is duplicated in '
+                                  f'{self.paramFilePath}{self.paramFileName}, value will be overwritten.')
+                        else:
+                            self.defaultValues[defaultValue['name']] = defaultValue['value']
+        except Exception as exc:
+            print(f'ERROR - An error occurred during parameters loading : {exc}')
+            raise RuntimeError from exc
+
+    def getSettings(self):
+        return self.settings
+
+    def getDefaultValues(self):
+        return self.defaultValues
+
+
+if __name__ == "__main__":
+    # test instance of the Parameters class
+    instance = Parameters()
+    print(instance)
+    print(instance.getDefaultValues().get('days_list')[1])
+    print(instance.getDefaultValues().get('help_message'))
